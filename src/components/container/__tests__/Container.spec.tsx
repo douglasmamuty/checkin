fcf0d45773228c000a8b76c1f93b92ae14381a7d
Douglas da Silva Saldanha <douglas.shusano@gmail.com>
2022-01-15 17:27:54 -0300

    Fixed the tests, to pass

--- src/components/container/__tests__/Container.spec.tsx
+++ src/components/container/__tests__/Container.spec.tsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { render, within } from '@testing-library/react';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { render, within } from "@testing-library/react";
 
-import theme from '../../../styles/theme';
-import Container from '../Container';
+import theme from "../../../styles/theme";
+import Container from "../Container";
 
-describe('Container Component', () => {
-  it('should be able to the render the Container component with children', () => {
-    const content = 'Test';
+describe("Container Component", () => {
+  it("should be able to the render the Container component with children", () => {
+    const content = "Test";
 
     const { getByTestId } = render(
       <ThemeProvider theme={theme}>
         <Container>{content}</Container>
-      </ThemeProvider>,
+      </ThemeProvider>
     );
 
-    const { getByText } = within(getByTestId('container-test'));
+    const { getByText } = within(getByTestId("container-test"));
 
     expect(getByText(content)).toBeInTheDocument();
   });
 
-  it('should be able to the render the Container component with custom padding', () => {
-    const padding = '40px';
+  it("should be able to the render the Container component with custom padding", () => {
+    const padding = "40px";
 
     const { getByTestId } = render(
       <ThemeProvider theme={theme}>
         <Container padding={padding}>Test</Container>
-      </ThemeProvider>,
+      </ThemeProvider>
     );
 
-    expect(getByTestId('container-test')).toHaveStyle(`padding: ${padding};`);
+    expect(getByTestId("container-test")).toHaveStyle(`padding: ${padding};`);
   });
 
-  it('should be able to the render the Container component with transparent background', () => {
+  it("should be able to the render the Container component with transparent background", () => {
     const { getByTestId } = render(
       <ThemeProvider theme={theme}>
         <Container transparent>Test</Container>
-      </ThemeProvider>,
+      </ThemeProvider>
     );
 
-    expect(getByTestId('container-test')).toHaveStyle(
-      `background-color: transparent;`,
+    expect(getByTestId("container-test")).toHaveStyle(
+      `background-color: transparent;`
     );
   });
 });
